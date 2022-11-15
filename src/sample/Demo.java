2676495bf32109348354a043595458c95f2e409c
HP <HP@LAPTOP-9C4QUDFT>
2022-11-15 23:27:28 +0530

    [Vikram]: added new project

---
+++ src/sample/Demo.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+}
